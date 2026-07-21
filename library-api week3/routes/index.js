f4b5951d53d1f0874a7f10a55013bd83ac51f2e9
Benedicta Etim <YOUR_GITHUB_EMAIL>
2026-07-21 03:41:36 +0100

    Fix routes by removing missing swagger module

--- library-api week3/routes/index.js
+++ library-api week3/routes/index.js
@@ -1,12 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.use('/', require('./swagger'));
-router.use('/books', require('./books'));
-router.use('/teams', require('./teams'));
-router.use('/authors', require('./authors'));
+router.use("/books", require("./books"));
+router.use("/teams", require("./teams"));
+router.use("/authors", require("./authors"));
 
-router.get('/', (req, res) => {
-    res.send('Welcome to the Library API');
+router.get("/", (req, res) => {
+  res.send("Welcome to the Library API");
 });
 
 module.exports = router;
