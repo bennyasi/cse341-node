8473dcfc9aab4ce8bf5a044b133269d401fd7eba
Benedicta Etim <YOUR_GITHUB_EMAIL>
2026-07-21 02:08:19 +0100

    Remove authors route from nested index.js

--- library-api week3/routes/index.js
+++ library-api week3/routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 router.use('/books', require('./books'));
-router.use('/authors', require('./authors'));
 
 router.get('/', (req, res) => {
     res.send('Welcome to the Library API');
