8a046f5c44462a8ca1893e86c182092f9bb0f7f8
Benedicta Etim <YOUR_GITHUB_EMAIL>
2026-07-23 18:16:56 +0100

    Update controller collection syntax

--- controllers/books.js
+++ controllers/books.js
@@ -3,7 +3,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllBooks = async (req, res, next) => {
   try {
-    const result = await mongodb.getDatabase().db().collection('books').find();
+    const result = await mongodb.getDatabase().collection('books').find();
     const books = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(books);
@@ -18,7 +18,7 @@ const getBookById = async (req, res, next) => {
       return res.status(400).json('Must use a valid book id to find a book.');
     }
     const bookId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('books').find({ _id: bookId });
+    const result = await mongodb.getDatabase().collection('books').find({ _id: bookId });
     const books = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(books[0]);
@@ -35,7 +35,7 @@ const createBook = async (req, res, next) => {
       year: req.body.year,
       genre: req.body.genre
     };
-    const response = await mongodb.getDatabase().db().collection('books').insertOne(book);
+    const response = await mongodb.getDatabase().collection('books').insertOne(book);
     if (response.acknowledged) {
       res.status(201).json(response);
     } else {
@@ -58,7 +58,7 @@ const updateBook = async (req, res, next) => {
       year: req.body.year,
       genre: req.body.genre
     };
-    const response = await mongodb.getDatabase().db().collection('books').replaceOne({ _id: bookId }, book);
+    const response = await mongodb.getDatabase().collection('books').replaceOne({ _id: bookId }, book);
     if (response.modifiedCount > 0) {
       res.status(204).send();
     } else {
@@ -75,7 +75,7 @@ const deleteBook = async (req, res, next) => {
       return res.status(400).json('Must use a valid book id to delete a book.');
     }
     const bookId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('books').deleteOne({ _id: bookId });
+    const response = await mongodb.getDatabase().collection('books').deleteOne({ _id: bookId });
     if (response.deletedCount > 0) {
       res.status(204).send();
     } else {
