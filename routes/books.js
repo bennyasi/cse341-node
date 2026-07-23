ba5915beef5497871f4b841e7d1092b645d137d9
Benedicta Etim <YOUR_GITHUB_EMAIL>
2026-07-23 17:58:09 +0100

    Fix controller and route configuration for books

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const router = require('express').Router();
+const booksController = require('../controllers/books');
 
-const booksController = require("../controllers/books");
-const validateBook = require("../middleware/validateBooks");
-
-router.get("/", booksController.getAllBooks);
-router.get("/:id", booksController.getSingleBook);
-router.post("/", validateBook, booksController.createBook);
-router.put("/:id", validateBook, booksController.updateBook);
-router.delete("/:id", booksController.deleteBook);
+router.get('/', booksController.getAllBooks);
+router.get('/:id', booksController.getBookById);
+router.post('/', booksController.createBook);
+router.put('/:id', booksController.updateBook);
+router.delete('/:id', booksController.deleteBook);
 
 module.exports = router;
